a60a7b371183cbddba973d96e7b7dcafc818055b
ZAYTSEV Andrey <andrey.zaytsev@arteliagroup.com>
2024-05-16 09:16:49 +0200

    Update initialisation config

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
-from openai import OpenAI
 from fastapi import FastAPI, HTTPException, Security, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
-
-import pinecone
+from openai import OpenAI
+from pinecone import Pinecone, ServerlessSpec
 
 load_dotenv()
 
-# uvicorn app:app --host 0.0.0.0 --port 10000
 app = FastAPI()
 
 # Setup environment variables
 openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
-# environment = os.getenv("PINECONE_ENV")
 index_name = os.getenv("PINECONE_INDEX")
 
 # Initialize pinecone client
-pinecone.init(api_key=pinecone_api_key)
+pinecone = Pinecone(api_key=pinecone_api_key)
+
+# Create or retrieve an index
+if index_name not in pinecone.list_indexes().names():
+    pinecone.create_index(
+        name=index_name,
+        dimension=1536,  # This number should match your embedding dimension
+        metric='euclidean',
+        spec=ServerlessSpec(
+            cloud='aws',  # or 'gcp' if using Google Cloud
+            region='us-west-2'  # or any other region
+        )
+    )
+
 index = pinecone.Index(index_name)
 
 # Middleware to secure HTTP endpoint
 security = HTTPBearer()
-
 
 def validate_token(
     http_auth_credentials: HTTPAuthorizationCredentials = Security(security),
@@ -37,10 +45,8 @@ def validate_token(
     else:
         raise HTTPException(status_code=403, detail="Invalid authentication scheme")
 
-
 class QueryModel(BaseModel):
     query: str
-
 
 @app.post("/")
 async def get_context(
@@ -49,14 +55,18 @@ async def get_context(
 ):
     # convert query to embeddings
     res = openai_client.embeddings.create(
-        input=[query_data.query], model="text-embedding-ada-002"
+        input=[query_data.query],
+        model="text-embedding-ada-002"
     )
     embedding = res.data[0].embedding
+
     # Search for matching Vectors
     results = index.query(embedding, top_k=6, include_metadata=True).to_dict()
-    # Filter out metadata fron search result
+
+    # Filter out metadata from search result
     context = [match["metadata"]["text"] for match in results["matches"]]
-    # Retrun context
+
+    # Return context
     return context
 
 
